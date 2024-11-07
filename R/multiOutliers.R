3c989d37bf07ba518d68a5f5b96695f467cd0299
Caleb Henning <chenning@wesleyan.edu>
2024-11-07 15:01:47 -0500

    changing na.rm portion

--- R/multiOutliers.R
+++ R/multiOutliers.R
@@ -22,7 +22,7 @@
 
 multiOutliers <- function(data, varlist=names(data), method, minPts=5, k=5, threshold =0.95, na.rm=TRUE, ...){
   #removing missing data
-  if(na.rm) data <- na.omit(data)
+  if(na.rm) data <- na.omit(data[,varlist])
 
   #add other methods as people finish them here
 
